739527f19cc89991531d0b02b149303a511c3b96
sarath684 <sarasiva684@gmail.com>
2024-06-24 13:52:15 +0530

    Add files via upload

---
+++ food_dict.py
@@ -0,0 +1,12 @@
+food_dictionary = {
+    'Italian': ['Margherita Pizza', 'Spaghetti Carbonara', 'Lasagna', 'Risotto', 'Tiramisu'],
+    'Indian': ['Butter Chicken', 'Palak Paneer', 'Biryani', 'Samosas', 'Dosa'],
+    'Chinese': ['Sweet and Sour Chicken', 'Kung Pao Chicken', 'Spring Rolls', 'Fried Rice', 'Dim Sum'],
+    'Spanish': ['Paella', 'Patatas Bravas', 'Churros', 'Gazpacho', 'Tortilla Española'],
+    'American': ['Cheeseburger', 'Buffalo Wings', 'Mac and Cheese', 'BBQ Ribs', 'Apple Pie'],
+    'Everything': ['Margherita Pizza', 'Spaghetti Carbonara', 'Lasagna', 'Risotto', 'Tiramisu', 'Butter Chicken',
+                   'Palak Paneer', 'Biryani', 'Samosas', 'Dosa', 'Sweet and Sour Chicken', 'Kung Pao Chicken',
+                   'Spring Rolls', 'Fried Rice', 'Dim Sum', 'Paella', 'Patatas Bravas', 'Churros', 'Gazpacho',
+                   'Tortilla Española', 'Cheeseburger', 'Buffalo Wings', 'Mac and Cheese', 'BBQ Ribs', 'Apple Pie']
+
+}
